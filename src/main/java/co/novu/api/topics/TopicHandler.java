237775ccedce742738d8a5da9848f28298121b37
Mukhtar <mukhtaronif@gmail.com>
2023-07-01 23:01:39 +0100

    Add Topic Impl

--- src/main/java/co/novu/api/topics/TopicHandler.java
+++ src/main/java/co/novu/api/topics/TopicHandler.java
@@ -5,7 +5,9 @@ import co.novu.api.topics.requests.FilterTopicsRequest;
 import co.novu.api.topics.requests.RenameTopicRequest;
 import co.novu.api.topics.requests.SubscriberAdditionRequest;
 import co.novu.api.topics.requests.TopicRequest;
-import co.novu.api.topics.responses.*;
+import co.novu.api.topics.responses.TopicResponse;
+import co.novu.api.topics.responses.SubscriberAdditionResponse;
+import co.novu.api.topics.responses.FilterTopicsResponse;
 import co.novu.common.base.NovuConfig;
 import co.novu.common.rest.RestHandler;
 
@@ -20,9 +22,10 @@ public class TopicHandler {
         this.restHandler = restHandler;
     }
 
+    private static final String ENDPOINT = "topics";
 
     public TopicResponse createTopic(TopicRequest request, NovuConfig novuConfig) {
-        return restHandler.handlePost(request, TopicResponse.class, novuConfig,"topics");
+        return restHandler.handlePost(request, TopicResponse.class, novuConfig,ENDPOINT);
     }
 
     public FilterTopicsResponse filterTopics(FilterTopicsRequest request, NovuConfig novuConfig) {
@@ -30,32 +33,32 @@ public class TopicHandler {
         if (request.getPage() != null) params.put("page", request.getPage());
         if (request.getPageSize() != null) params.put("pageSize", request.getPageSize());
         if (request.getKey() != null) params.put("key", request.getKey());
-        return restHandler.handleGet(FilterTopicsResponse.class, novuConfig,"topics", params);
+        return restHandler.handleGet(FilterTopicsResponse.class, novuConfig,ENDPOINT, params);
     }
 
 
     public SubscriberAdditionResponse addSubscriberToTopic(SubscriberAdditionRequest request, String topicKey,NovuConfig novuConfig) {
-        return restHandler.handlePost(request, SubscriberAdditionResponse.class, novuConfig,"topics/"+ topicKey+"/subscribers");
+        return restHandler.handlePost(request, SubscriberAdditionResponse.class, novuConfig,ENDPOINT + "/" + topicKey +"/subscribers");
     }
 
     public TopicResponse checkTopicSubscriber(String topicKey,String externalSubscriberId,NovuConfig novuConfig) {
-        return restHandler.handleGet(TopicResponse.class, novuConfig,"topics/"+topicKey+"/subscribers/"+externalSubscriberId);
+        return restHandler.handleGet(TopicResponse.class, novuConfig,ENDPOINT+ "/" +topicKey+"/subscribers/"+externalSubscriberId);
     }
     public Void removeSubscriberFromTopic(SubscriberAdditionRequest request, String topicKey, NovuConfig novuConfig) {
-        return restHandler.handlePost(request,Void.class, novuConfig,"topics/"+topicKey+"/subscribers/removal");
+        return restHandler.handlePost(request,Void.class, novuConfig,ENDPOINT+ "/" +topicKey+"/subscribers/removal");
     }
 
     public Void deleteTopic( String topicKey, NovuConfig novuConfig) {
-        return restHandler.handleDelete(Void.class, novuConfig,"topics/" +topicKey);
+        return restHandler.handleDelete(Void.class, novuConfig,ENDPOINT + "/" +topicKey);
     }
 
 
     public TopicResponse getTopic(String topicKey, NovuConfig novuConfig) {
-        return restHandler.handleGet(TopicResponse.class, novuConfig,"topics/" +topicKey);
+        return restHandler.handleGet(TopicResponse.class, novuConfig,ENDPOINT + "/" +topicKey);
     }
 
     public TopicResponse renameTopic(RenameTopicRequest request,String topicKey, NovuConfig novuConfig) {
-        return restHandler.handlePatch(request,TopicResponse.class, novuConfig,"topics/" +topicKey);
+        return restHandler.handlePatch(request,TopicResponse.class, novuConfig,ENDPOINT + "/" +topicKey);
     }
 
 
