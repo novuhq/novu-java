ad8fb65686334f2ea0ad256e71f8fbd758137204
piyushchhabra <maniisback@rediffmail.com>
2023-10-04 16:14:33 +0530

    Layouts API refactored by using retrofit

--- src/main/java/co/novu/api/layouts/LayoutHandler.java
+++ src/main/java/co/novu/api/layouts/LayoutHandler.java
@@ -7,60 +7,60 @@ import co.novu.api.layouts.responses.GetLayoutResponse;
 import co.novu.api.layouts.responses.CreateLayoutResponse;
 import co.novu.api.layouts.responses.FilterLayoutResponse;
 import co.novu.api.layouts.responses.SetDefaultLayoutResponse;
-import co.novu.common.base.NovuConfig;
+import co.novu.common.rest.NovuNetworkException;
 import co.novu.common.rest.RestHandler;
-import lombok.RequiredArgsConstructor;
+import retrofit2.Response;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-@RequiredArgsConstructor
 public class LayoutHandler {
 
     private final RestHandler restHandler;
 
-    private final NovuConfig novuConfig;
-
     private static final String ENDPOINT = "layouts";
 
-    public CreateLayoutResponse createLayout(LayoutRequest request) {
-        return restHandler.handlePost(request, CreateLayoutResponse.class, novuConfig, ENDPOINT);
+    private final LayoutApi layoutApi;
+
+    public LayoutHandler(RestHandler restHandler) {
+        this.restHandler = restHandler;
+        this.layoutApi = restHandler.buildRetrofit().create(LayoutApi.class);
     }
 
-    public FilterLayoutResponse filterLayouts(FilterLayoutRequest request) {
+    public CreateLayoutResponse createLayout(LayoutRequest request) throws IOException, NovuNetworkException {
+        Response<CreateLayoutResponse> response = layoutApi.createLayout(request).execute();
+        return restHandler.extractResponse(response);
+    }
+
+    public FilterLayoutResponse filterLayouts(FilterLayoutRequest request) throws IOException, NovuNetworkException {
         Map<String, Object> params = new HashMap<>();
         if (request.getPage() != null) params.put("page", request.getPage());
         if (request.getPageSize() != null) params.put("pageSize", request.getPageSize());
         if (request.getSortBy() != null) params.put("sortBy", request.getSortBy());
         if (request.getOrderBy() != null) params.put("orderBy", request.getOrderBy());
 
-        if (params.isEmpty()) {
-            return restHandler.handleGet(FilterLayoutResponse.class, novuConfig, ENDPOINT);
-        }
-        return restHandler.handleGet(FilterLayoutResponse.class, novuConfig, ENDPOINT, params);
+        Response<FilterLayoutResponse> response = layoutApi.filterLayouts(params).execute();
+        return restHandler.extractResponse(response);
     }
 
-    public GetLayoutResponse getLayout(String layoutId) {
-        return restHandler.handleGet(GetLayoutResponse.class, novuConfig, ENDPOINT  + "/" + layoutId);
+    public GetLayoutResponse getLayout(String layoutId) throws IOException, NovuNetworkException {
+        Response<GetLayoutResponse> response = layoutApi.getLayout(layoutId).execute();
+        return restHandler.extractResponse(response);
     }
 
-    public DeleteLayoutResponse deleteLayout(String layoutId) {
-        boolean isSuccess = restHandler.handleDeleteForVoid(novuConfig, ENDPOINT + "/" + layoutId);
-        if (isSuccess) {
-            return new DeleteLayoutResponse();
-        }
-        return null;
+    public DeleteLayoutResponse deleteLayout(String layoutId) throws IOException, NovuNetworkException {
+        Response<DeleteLayoutResponse> response = layoutApi.deleteLayout(layoutId).execute();
+        return restHandler.extractResponse(response, new DeleteLayoutResponse());
     }
 
-    public GetLayoutResponse updateLayout(String layoutId, LayoutRequest request) {
-        return restHandler.handlePatch(request, GetLayoutResponse.class, novuConfig, ENDPOINT + "/" + layoutId);
+    public GetLayoutResponse updateLayout(String layoutId, LayoutRequest request) throws IOException, NovuNetworkException {
+        Response<GetLayoutResponse> response = layoutApi.updateLayout(layoutId, request).execute();
+        return restHandler.extractResponse(response);
     }
 
-    public SetDefaultLayoutResponse setDefaultLayout(String layoutId) {
-        boolean isSuccess = restHandler.handlePostForVoid(novuConfig, ENDPOINT + "/" + layoutId + "/default");
-        if (isSuccess) {
-            return new SetDefaultLayoutResponse();
-        }
-        return null;
+    public SetDefaultLayoutResponse setDefaultLayout(String layoutId) throws IOException, NovuNetworkException {
+        Response<SetDefaultLayoutResponse> response = layoutApi.setDefaultLayout(layoutId).execute();
+        return restHandler.extractResponse(response, new SetDefaultLayoutResponse());
     }
 }
