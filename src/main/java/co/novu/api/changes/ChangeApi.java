438452e48c77f7e5f72337e22358de29db5868d8
piyushchhabra <maniisback@rediffmail.com>
2023-10-07 15:39:28 +0530

    removed wildcard imports

--- src/main/java/co/novu/api/changes/ChangeApi.java
+++ src/main/java/co/novu/api/changes/ChangeApi.java
@@ -5,7 +5,11 @@ import co.novu.api.changes.responses.ApplyChangesResponse;
 import co.novu.api.changes.responses.ChangeCountResponse;
 import co.novu.api.changes.responses.GetChangesResponse;
 import retrofit2.Call;
-import retrofit2.http.*;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.QueryMap;
 
 import java.util.Map;
 
