0bcdcb3d9cfcf0ab1a940fefeba93f2da9d9a7d1
sccalabr <sccalabr@calpoly.edu>
2023-10-08 16:26:24 -0700

    feat: Refactor Integrations API
    Refactoring Integrations API to use REST. See #56

--- src/test/java/co/novu/api/integrations/IntegrationsHandlerTest.java
+++ src/test/java/co/novu/api/integrations/IntegrationsHandlerTest.java
@@ -1,45 +1,57 @@
 package co.novu.api.integrations;
 
+import java.io.IOException;
+import java.util.Collections;
+
+import com.google.gson.Gson;
+
 import co.novu.api.integrations.pojo.Integration;
 import co.novu.api.integrations.requests.IntegrationRequest;
 import co.novu.api.integrations.responses.BulkIntegrationResponse;
 import co.novu.api.integrations.responses.ProviderWebhookStatusResponse;
 import co.novu.api.integrations.responses.SingleIntegrationResponse;
 import co.novu.common.base.NovuConfig;
+import co.novu.common.rest.NovuNetworkException;
 import co.novu.common.rest.RestHandler;
 import junit.framework.TestCase;
-import org.mockito.Mockito;
-
-import java.util.Collections;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
 
 public class IntegrationsHandlerTest extends TestCase {
 
     private IntegrationsHandler integrationsHandler;
 
-    private RestHandler restHandler;
+    private MockWebServer mockWebServer;
 
     @Override
     protected void setUp() {
-        restHandler = Mockito.mock(RestHandler.class);
-        NovuConfig novuConfig = Mockito.mock(NovuConfig.class);
-        integrationsHandler = Mockito.spy(new IntegrationsHandler(restHandler, novuConfig));
+        mockWebServer = new MockWebServer();
+        NovuConfig novuConfig = new NovuConfig("1234");
+        novuConfig.setBaseUrl(mockWebServer.url("").toString());
+        RestHandler restHandler = new RestHandler(novuConfig);
+        integrationsHandler = new IntegrationsHandler(restHandler);
     }
 
-    public void test_getIntegrations() {
+    public void test_getIntegrations() throws NovuNetworkException, IOException, InterruptedException {
         BulkIntegrationResponse bulkIntegrationResponse = new BulkIntegrationResponse();
         Integration integration = new Integration();
         integration.setActive(true);
         integration.setChannel("PUSH");
         bulkIntegrationResponse.setData(Collections.singletonList(integration));
 
-        Mockito.doReturn(bulkIntegrationResponse).when(restHandler).handleGet(Mockito.any(), Mockito.any(), Mockito.any());
+        Gson gson = new Gson();
+        mockWebServer.enqueue(new MockResponse().setResponseCode(200).setBody(gson.toJson(bulkIntegrationResponse)));
 
         BulkIntegrationResponse response = integrationsHandler.getIntegrations();
-        assertNotNull(response);
-        assertEquals(bulkIntegrationResponse, response);
+        RecordedRequest request = mockWebServer.takeRequest();
+
+        assertEquals("/integrations/get-integrations", request.getPath());
+        assertEquals("GET", request.getMethod());
+        assertEquals(gson.toJson(bulkIntegrationResponse), gson.toJson(response));
     }
 
-    public void test_createIntegration() {
+    public void test_createIntegration() throws NovuNetworkException, IOException, InterruptedException {
         IntegrationRequest integrationRequest = new IntegrationRequest();
         integrationRequest.setActive(true);
         integrationRequest.setChannel("PUSH");
@@ -51,39 +63,51 @@ public class IntegrationsHandlerTest extends TestCase {
         integration.setChannel("PUSH");
         singleIntegrationResponse.setData(integration);
 
-        Mockito.doReturn(singleIntegrationResponse).when(restHandler).handlePost(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any());
+        Gson gson = new Gson();
+        mockWebServer.enqueue(new MockResponse().setResponseCode(200).setBody(gson.toJson(singleIntegrationResponse)));
 
         SingleIntegrationResponse response = integrationsHandler.createIntegration(integrationRequest);
-        assertNotNull(response);
-        assertEquals(singleIntegrationResponse, response);
+        RecordedRequest request = mockWebServer.takeRequest();
+
+        assertEquals("/integrations/create-integrations/IntegrationRequest(providerId=pId,%20channel=PUSH,%20credentials=null,%20active=true,%20check=null)", request.getPath());
+        assertEquals("POST", request.getMethod());
+        assertEquals(gson.toJson(singleIntegrationResponse), gson.toJson(response));
     }
 
-    public void test_getActiveIntegrations() {
+    public void test_getActiveIntegrations() throws NovuNetworkException, IOException, InterruptedException {
         BulkIntegrationResponse bulkIntegrationResponse = new BulkIntegrationResponse();
         Integration integration = new Integration();
         integration.setActive(true);
         integration.setChannel("PUSH");
         bulkIntegrationResponse.setData(Collections.singletonList(integration));
 
-        Mockito.doReturn(bulkIntegrationResponse).when(restHandler).handleGet(Mockito.any(), Mockito.any(), Mockito.any());
+        Gson gson = new Gson();
+        mockWebServer.enqueue(new MockResponse().setResponseCode(200).setBody(gson.toJson(bulkIntegrationResponse)));
 
         BulkIntegrationResponse response = integrationsHandler.getActiveIntegrations();
-        assertNotNull(response);
-        assertEquals(bulkIntegrationResponse, response);
+        RecordedRequest request = mockWebServer.takeRequest();
+
+        assertEquals("/integrations/active", request.getPath());
+        assertEquals("GET", request.getMethod());
+        assertEquals(gson.toJson(bulkIntegrationResponse), gson.toJson(response));
     }
 
-    public void test_getProviderWebhookStatus() {
+    public void test_getProviderWebhookStatus() throws NovuNetworkException, IOException, InterruptedException {
         ProviderWebhookStatusResponse bulkIntegrationResponse = new ProviderWebhookStatusResponse();
         bulkIntegrationResponse.setData(false);
 
-        Mockito.doReturn(bulkIntegrationResponse).when(restHandler).handleGet(Mockito.any(), Mockito.any(), Mockito.any());
+        Gson gson = new Gson();
+        mockWebServer.enqueue(new MockResponse().setResponseCode(200).setBody(gson.toJson(bulkIntegrationResponse)));
 
-        ProviderWebhookStatusResponse response = integrationsHandler.getProviderWebhookStatus("id");
-        assertNotNull(response);
-        assertEquals(bulkIntegrationResponse, response);
+        ProviderWebhookStatusResponse response = integrationsHandler.getProviderWebhookStatus("Test");
+        RecordedRequest request = mockWebServer.takeRequest();
+
+        assertEquals("/integrations/webhook/provider/Test/status", request.getPath());
+        assertEquals("GET", request.getMethod());
+        assertEquals(gson.toJson(bulkIntegrationResponse), gson.toJson(response));
     }
 
-    public void test_updateIntegration() {
+    public void test_updateIntegration() throws NovuNetworkException, IOException, InterruptedException {
         IntegrationRequest integrationRequest = new IntegrationRequest();
         integrationRequest.setActive(true);
         integrationRequest.setChannel("PUSH");
@@ -95,39 +119,53 @@ public class IntegrationsHandlerTest extends TestCase {
         integration.setChannel("PUSH");
         singleIntegrationResponse.setData(integration);
 
-        Mockito.doReturn(singleIntegrationResponse).when(restHandler).handlePut(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any());
+        Gson gson = new Gson();
+        mockWebServer.enqueue(new MockResponse().setResponseCode(200).setBody(gson.toJson(singleIntegrationResponse)));
 
-        SingleIntegrationResponse response = integrationsHandler.updateIntegration("id", integrationRequest);
-        assertNotNull(response);
-        assertEquals(singleIntegrationResponse, response);
+        SingleIntegrationResponse response = integrationsHandler.updateIntegration("Test", integrationRequest);
+        RecordedRequest request = mockWebServer.takeRequest();
+
+        assertEquals("/integrations/Test/IntegrationRequest(providerId=pId,%20channel=PUSH,%20credentials=null,%20active=true,%20check=null)", request.getPath());
+        assertEquals("PUT", request.getMethod());
+        assertEquals(gson.toJson(singleIntegrationResponse), gson.toJson(response));
     }
 
-    public void test_deleteIntegration() {
+    public void test_deleteIntegration() throws NovuNetworkException, IOException, InterruptedException {
         BulkIntegrationResponse bulkIntegrationResponse = new BulkIntegrationResponse();
         Integration integration = new Integration();
         integration.setActive(true);
         integration.setChannel("PUSH");
+        integration.setId("Test");
         bulkIntegrationResponse.setData(Collections.singletonList(integration));
 
-        Mockito.doReturn(bulkIntegrationResponse).when(restHandler).handleDelete(Mockito.any(), Mockito.any(), Mockito.any());
+        Gson gson = new Gson();
+        mockWebServer.enqueue(new MockResponse().setResponseCode(200).setBody(gson.toJson(bulkIntegrationResponse)));
 
-        BulkIntegrationResponse response = integrationsHandler.deleteIntegration("id");
-        assertNotNull(response);
-        assertEquals(bulkIntegrationResponse, response);
+        BulkIntegrationResponse response = integrationsHandler.deleteIntegration(integration.getId());
+        RecordedRequest request = mockWebServer.takeRequest();
+
+        assertEquals("/integrations/Test", request.getPath());
+        assertEquals("DELETE", request.getMethod());
+        assertEquals(gson.toJson(bulkIntegrationResponse), gson.toJson(response));
     }
 
-    public void test_setIntegrationAsPrimary() {
+    public void test_setIntegrationAsPrimary() throws NovuNetworkException, IOException, InterruptedException {
         SingleIntegrationResponse singleIntegrationResponse = new SingleIntegrationResponse();
         Integration integration = new Integration();
         integration.setActive(true);
         integration.setChannel("PUSH");
         integration.setPrimary(true);
+        integration.setId("Test");
         singleIntegrationResponse.setData(integration);
 
-        Mockito.doReturn(singleIntegrationResponse).when(restHandler).handlePost(Mockito.any(), Mockito.any(), Mockito.any());
+        Gson gson = new Gson();
+        mockWebServer.enqueue(new MockResponse().setResponseCode(200).setBody(gson.toJson(singleIntegrationResponse)));
 
-        SingleIntegrationResponse response = integrationsHandler.setIntegrationAsPrimary("id");
-        assertNotNull(response);
-        assertEquals(singleIntegrationResponse, response);
+        SingleIntegrationResponse response = integrationsHandler.setIntegrationAsPrimary(integration.getId());
+        RecordedRequest request = mockWebServer.takeRequest();
+
+        assertEquals("/integrations/Test/set-primary", request.getPath());
+        assertEquals("PUT", request.getMethod());
+        assertEquals(gson.toJson(singleIntegrationResponse), gson.toJson(response));
     }
 }
