88a4772dab9b3d7f83bad09c91935225d1abf625
ChenemiKen <chenemiken15@gmail.com>
2023-10-10 20:35:56 +0100

    included missing @body and @path anotations

--- src/main/java/co/novu/api/events/EventsApi.java
+++ src/main/java/co/novu/api/events/EventsApi.java
@@ -6,21 +6,23 @@ import co.novu.api.events.responses.BulkTriggerEventResponse;
 import co.novu.api.events.responses.CancelEventResponse;
 import co.novu.api.events.responses.TriggerEventResponse;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.DELETE;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface EventsApi {
     String ENDPOINT = "events/trigger";
 
     @POST(ENDPOINT)
-    Call<TriggerEventResponse> triggerEvent(TriggerEventRequest request);
+    Call<TriggerEventResponse> triggerEvent(@Body TriggerEventRequest request);
 
     @POST(ENDPOINT + "/bulk")
-    Call<BulkTriggerEventResponse> bulkTriggerEvent(BulkTriggerEventRequest request);
+    Call<BulkTriggerEventResponse> bulkTriggerEvent(@Body BulkTriggerEventRequest request);
 
     @POST(ENDPOINT + "/broadcast")
-    Call<TriggerEventResponse> broadcastEvent(TriggerEventRequest request);
+    Call<TriggerEventResponse> broadcastEvent(@Body TriggerEventRequest request);
 
-    @DELETE(ENDPOINT + "/{{transactionId}}")
-    Call<CancelEventResponse> cancelTriggeredEvent(String transactionId);
+    @DELETE(ENDPOINT + "/{transactionId}")
+    Call<CancelEventResponse> cancelTriggeredEvent(@Path("transactionId") String transactionId);
 }
